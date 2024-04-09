f992fe22c563f0f76755878f47f3c89942feed1e
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-04-08 22:49:57 -0600

    Actualiza la configuración de correo electrónico en emailConfig.js

--- src/auth/email/emailConfig.js
+++ src/auth/email/emailConfig.js
@@ -1,6 +1,10 @@
 const nodemailer = require("nodemailer");
 const axios = require("axios");
 
+process.loadEnvFile();
+
+const { GMAIL_USER, GMAIL_CLIENT_ID, GMAIL_CLIENT_SECRET, GMAIL_REFRESH_TOKEN } = process.env;
+
 const authData = {
     type: 'OAuth2',
     user: 'greenroofoficial@gmail.com',
